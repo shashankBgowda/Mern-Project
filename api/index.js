be7ab9c05308e4f69b80a024b8feff688b74b2a2
shashankBgowda <shashankbg74@gmail.com>
2025-02-20 16:25:50 -0500

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to MongoDB...");
+}).catch((err)=>{
+    console.log(err);
+});
 const app = express();
-app.listen(4500,()=>{
-    console.log("server listning to port 4590");
+app.listen(5000,()=>{
+    console.log("server listning to port 5000");
 })
